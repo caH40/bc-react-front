aa27efaf41c9e8c37b9e8ce4b519c78823ed38fc
Alexander Berezhnev <freelancerbav@gmail.com>
2023-01-31 16:56:25 +0300

    added a cross to close the modal

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { getModal } from '../../redux/features/modalSlice';
+import ButtonClose from '../UI/ButtonClose/ButtonClose';
 
 import classes from './Modal.module.css';
 
 const Modal = ({ children }) => {
+	const getClick = () => dispatch(getModal({ components: '' }));
+
 	const dispatch = useDispatch();
 	window.addEventListener('keydown', keyHandler);
 
@@ -16,7 +19,10 @@ const Modal = ({ children }) => {
 	return (
 		<div className={classes.background}>
 			<div className={classes.inner}>
-				<div className={classes.block}>{children}</div>
+				<div className={classes.block}>
+					<ButtonClose getClick={getClick} />
+					{children}
+				</div>
 			</div>
 		</div>
 	);
