842ac499b9e5fbf1b1c6b79374c779449fec62d0
Alexander Berezhnev <freelancerbav@gmail.com>
2023-02-08 16:43:32 +0300

    getting a picture for fullnews by link from the server

--- src/Pages/NewsFull.jsx
+++ src/Pages/NewsFull.jsx
@@ -6,6 +6,8 @@ import HNewsFull from '../Components/Helmets/HNewsFull';
 import NewsInteractive from '../Components/NewsInteractive/NewsInteractive';
 import classes from './PagesCss/NewsFull.module.css';
 
+const server = process.env.REACT_APP_SERVER_EXPRESS;
+
 const NewsFull = () => {
 	const [news, setNews] = useState({});
 	const { newsId } = useParams();
@@ -15,12 +17,12 @@ const NewsFull = () => {
 	}, [newsId]);
 	return (
 		<>
-			{news._id ? (
+			{news?._id ? (
 				<div className={classes.container}>
 					<HNewsFull news={news} />
 					<h1 className={classes.title}>{news.newsTitle}</h1>
 					<div className={classes.date}>{new Date(news.date).toLocaleString()}</div>
-					<img className={classes.img} src={news.newsImage} alt={news.newsTitle} />
+					<img className={classes.img} src={`${server}/${news?.image}`} alt={news.newsTitle} />
 					<div className={classes.text} dangerouslySetInnerHTML={{ __html: news.newsText }}></div>
 					<NewsInteractive newsOne={news} />
 				</div>
