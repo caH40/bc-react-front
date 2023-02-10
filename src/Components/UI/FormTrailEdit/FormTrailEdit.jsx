7eabc664e90acc68c12aebbd4788f553165b9815
Alexander Berezhnev <freelancerbav@gmail.com>
2023-02-10 09:07:23 +0300

    minor fixes

--- src/Components/UI/FormTrailEdit/FormTrailEdit.jsx
+++ src/Components/UI/FormTrailEdit/FormTrailEdit.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import ImagesURLBox from '../../ImagesURLBox copy/ImagesURLBox';
+import ImagesURLBox from '../../ImagesURLBox/ImagesURLBox';
 import ImageURLBox from '../../ImageURLBox/ImageURLBox';
 
 import InputBox from '../InputBox/InputBox';
