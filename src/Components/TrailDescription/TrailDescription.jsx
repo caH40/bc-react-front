06c0c5d2975e6a49c71cf3252e8ed114ac9e53d1
Alexander Berezhnev <freelancerbav@gmail.com>
2023-02-10 17:51:52 +0300

    fixed bug with photosId

--- src/Components/TrailDescription/TrailDescription.jsx
+++ src/Components/TrailDescription/TrailDescription.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 import classes from './TrailDescription.module.css';
 
 const TrailDescription = ({ trail }) => {
+	console.log(trail);
 	return (
 		<div className={classes.block__description}>
 			{trail.descriptionArea.map((paragraph, index) => {
 				if (paragraph === '') return '';
 				return (
-					<div className={classes.box__description} key={trail.descPhotoId + index}>
+					<div className={classes.box__description} key={trail.descPhotos + index}>
 						<p className={classes.text}>{paragraph}</p>
 						<img
 							src={trail.descPhoto[index]}
