98cabd19f4deb4708d6a9603d6dc847081fcc98e
Alexander Berezhnev <freelancerbav@gmail.com>
2023-02-12 13:05:15 +0300

    added commetCreate components

--- src/Components/UI/CommentBlock/CommentBlock.jsx
+++ src/Components/UI/CommentBlock/CommentBlock.jsx
@@ -4,6 +4,7 @@ import { getComments, postCommentNews, postCommentDelete } from '../../../api/co
 import autosize from 'autosize';
 
 import classes from './CommentBlock.module.css';
+import CommentCreate from '../CommentCreate/CommentCreate';
 
 const CommentBlock = ({ newsId }) => {
 	const [comment, setComment] = useState('');
@@ -41,41 +42,13 @@ const CommentBlock = ({ newsId }) => {
 
 	return (
 		<div className={classes.wrapper}>
-			<div className={classes.create}>
-				<div className={classes.box__avatar}>
-					<img
-						className={classes.avatar}
-						src={authUser.user.photoProfile || '/images/avatar.svg'}
-						alt="avatar"
-					/>
-				</div>
-				<textarea
-					ref={textAreaRef}
-					value={comment}
-					onChange={e => setComment(e.target.value)}
-					className={classes.textarea}
-					placeholder="Комментарий"
-				/>
-				{comment ? (
-					<div onClick={sendComment} className={classes.box__send}>
-						<svg
-							className={classes.send}
-							width="28"
-							height="21"
-							viewBox="0 0 28 21"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M2 2L25 10.5427L2 19L5.5 10.5427L2 2Z"
-								fill="#FF8C06"
-								stroke="#FF8C06"
-								strokeWidth="2"
-							/>
-						</svg>
-					</div>
-				) : undefined}
-			</div>
+			<CommentCreate
+				textAreaRef={textAreaRef}
+				authUser={authUser}
+				sendComment={sendComment}
+				comment={comment}
+				setComment={setComment}
+			/>
 
 			{comments.map(commentOne => {
 				const id = commentOne._id;
---
+++ src/Components/UI/CommentCreate/CommentCreate.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import classes from './CommentCreate.module.css';
+
+const CommentCreate = ({ textAreaRef, authUser, comment, setComment, sendComment }) => {
+	return (
+		<div className={classes.create}>
+			<div className={classes.box__avatar}>
+				<img
+					className={classes.avatar}
+					src={authUser.user.photoProfile || '/images/avatar.svg'}
+					alt="avatar"
+				/>
+			</div>
+			<textarea
+				ref={textAreaRef}
+				value={comment}
+				onChange={e => setComment(e.target.value)}
+				className={classes.textarea}
+				placeholder="Комментарий"
+			/>
+			{comment ? (
+				<div onClick={sendComment} className={classes.box__send}>
+					<svg
+						className={classes.send}
+						width="28"
+						height="21"
+						viewBox="0 0 28 21"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M2 2L25 10.5427L2 19L5.5 10.5427L2 2Z"
+							fill="#FF8C06"
+							stroke="#FF8C06"
+							strokeWidth="2"
+						/>
+					</svg>
+				</div>
+			) : undefined}
+		</div>
+	);
+};
+
+export default CommentCreate;
