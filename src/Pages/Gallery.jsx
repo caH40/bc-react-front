71bc87b15505c0a970da882676a4898cc695c141
Alexander Berezhnev <freelancerbav@gmail.com>
2023-03-02 09:43:04 +0300

    init the gallery

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Album from '../Components/Album/Album';
 import HGallery from '../Components/Helmets/HGallery';
+
+import classes from './PagesCss/Gallery.module.css';
 
 const Gallery = () => {
 	return (
 		<div>
 			<HGallery />
-			Gallery
+			<h1 className={classes.title}>Gallery</h1>
+			<Album />
 		</div>
 	);
 };
